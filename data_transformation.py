98b891ecfa07da42bc41b62304038e9114d5952b
Berkay <berkay.bb.bozkurt@gmail.com>
2023-06-06 15:11:41 +0200

    added bunch of files

---
+++ data_transformation.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from scipy.sparse import csr_matrix
+
+def data_preparation():
+    """
+    Perform data preparation by reading movie and ratings data, merging them, calculating average ratings,
+    filtering for popular movies, and returning the prepared DataFrame.
+    """
+    df_mov = pd.read_csv("./data/movies.csv")
+    df_user = pd.read_csv("./data/ratings.csv")
+    
+    df = df_mov.merge(df_user, on=["movieId"])
+
+    df["av_rating"] = df.groupby("title")["rating"].transform("mean")
+
+    # calculate the number of ratings per movie
+    rating_count = df.groupby('movieId')[['rating']].count()
+    
+    # filter for movies with more than 20 ratings and extract the index
+    popular_movies = rating_count[rating_count['rating'] > 20].index
+    
+    # filter the ratings matrix and only keep the popular movies
+    df = df[df['movieId'].isin(popular_movies)].copy()
+    
+    return df
+
+def data_transformations(df):
+    """
+    Perform data transformations by mapping user and movie IDs, sorting the DataFrame,
+    and extracting the list of movie titles.
+    """
+    # remap user and movie IDs to sequential integers
+    user_ids = df['userId'].unique()
+    user_id_map = {v: k for k, v in enumerate(user_ids)}
+    df['userId'] = df['userId'].map(user_id_map)
+    
+    movie_ids = df['movieId'].unique()
+    movie_id_map = {v: k for k, v in enumerate(movie_ids)}
+    df['movieId'] = df['movieId'].map(movie_id_map)
+    
+    # sort the DataFrame by average rating in descending order
+    df.sort_values(by="av_rating", ascending=False, inplace=True)
+    
+    # extract the list of movie titles
+    df_grp = df.groupby("title")[["av_rating"]].first()
+    movies = df_grp.index.tolist()
+    
+    return df, movies
+
+def create_csr_matrix(df):
+    """
+    Create a CSR matrix from the DataFrame containing user ratings.
+    """
+    R = csr_matrix((df['rating'], (df['userId'], df['movieId'])))
+    Rt = R.todense()
+    return Rt
+
+# Perform data preparation and transformations, and create the CSR matrix
+df = data_preparation()
+df, movies = data_transformations(df)
+Rt = create_csr_matrix(df)
--- recommenders.py
+++ recommenders.py
@@ -3,31 +3,110 @@ In this script we define functions for the recommender web
 application
 """
 import random
-from utils import MOVIES, nmf_model, cos_sim_model
+import pandas as pd
+import numpy as np
+from data_transformation import df, movies, Rt
 
 
-def nmf_recommender(query, nmf_model, titles, k=10):
-    """This is an nmf-based recommender"""
-    return NotImplementedError
+def recommend_nmf(query, model):
+    """
+    Filters and recommends the top k movies for any given input query based on a trained NMF model. 
+    Returns a list of k movie ids.
+    
+    Args:
+        query (dict): A dictionary containing movie ratings for the query user.
+        model: The trained NMF model.
+    
+    Returns:
+        list: The top k movie ids recommended for the query user.
+    """
+    
+    # 1. Construct new_user-item dataframe given the query
+    Q_matrix = model.components_
+    Q = pd.DataFrame(Q_matrix, columns=movies, index=model.get_feature_names_out())
+    new_user_dataframe = pd.DataFrame(query, columns=movies, index=["new_user"])
+    #df["av_rating"] = df.groupby("userId")["rating"].transform("mean")
+    df_grp = df.groupby("title")[["av_rating"]].first()
+    new_user_dataframe_imputed = new_user_dataframe.fillna(0)
+    #new_user_dataframe_imputed = new_user_dataframe.fillna(sum(query.values())/len(query.values()))
+    #new_user_dataframe_imputed = new_user_dataframe.fillna(df[df["userId"]==12]["av_rating"].to_list()[0])
+    """
+    df_12=df[df["userId"]==12]
+    dict(zip(df_12.title, df_12.rating))
+    """
+   
+    # 2. Scoring
+    # Calculate the score with the NMF model
+    P_new_user_matrix = model.transform(new_user_dataframe_imputed)
+    P_new_user = pd.DataFrame(P_new_user_matrix, columns=model.get_feature_names_out(), index=['new_user'])
+    R_hat_new_user_matrix = np.dot(P_new_user, Q)
+    R_hat_new_user = pd.DataFrame(data=R_hat_new_user_matrix, columns=movies, index=['new_user'])    
 
-def cos_sim_recommender(query, cos_sim_model, titles, k=10):
-    """This is an cosine-similarity-based recommender"""
-    return NotImplementedError
+    # 3. Ranking
+    # Filter out movies already seen by the user
+    R_hat_new_user_filtered = R_hat_new_user.drop(query.keys(), axis=1)
+    nmf_score = R_hat_new_user_filtered.sum()
+    return nmf_score
 
 
-def random_recommender(k=2):
-    if k > len(MOVIES):
-        print(f"Hey you exceed the allowed number of movies {len(MOVIES)}")
-        return []
-    else:
-        random.shuffle(MOVIES)
-        top_k = MOVIES[:k]
-        return top_k
+def recommend_neighborhood(query, model, ratings):
+    """
+    Filters and recommends the top k movies for any given input query based on a trained nearest neighbors model. 
+    Returns a list of k movie ids.
+    
+    Args:
+        query (dict): A dictionary containing movie ratings for the query user.
+        model: The trained nearest neighbors model.
+        ratings (int): The number of neighbors to consider for recommendation.
+    
+    Returns:
+        list: The top k movie ids recommended for the query user.
+    """
+    # 1. Candidate generation
+    # Construct a user vector
+    new_user_dataframe = pd.DataFrame(query, columns=movies, index=['new_user'])
+    df_grp = df.groupby("title")[["av_rating"]].first()
+    #df["av_rating"] = df.groupby("userId")["rating"].transform("mean")
+    new_user_dataframe_imputed = new_user_dataframe.fillna(0)
+    #new_user_dataframe_imputed = new_user_dataframe.fillna(sum(query.values())/len(query.values()))
+    #new_user_dataframe_imputed = new_user_dataframe.fillna(df[df["userId"]==12]["av_rating"].to_list()[0])
+    """
+    df_12=df[df["userId"]==12]
+    dict(zip(df_12.title, df_12.rating))
+    """
+   
+    # 2. Scoring
+    # Find n neighbors
+    similarity_scores, neighbor_ids = model.kneighbors(new_user_dataframe_imputed,
+                                                       n_neighbors=ratings,
+                                                       return_distance=True)
+
+    neighborhood = pd.DataFrame(Rt, columns=movies).iloc[neighbor_ids[0]]
+    
+    # Calculate their average rating
+    neighborhood_filtered = neighborhood.drop(query.keys(), axis=1)
+    df_score = neighborhood_filtered.sum() / ratings
+    return df_score
 
 
-if __name__ == "__main__":
-    top2 = random_recommender()
-    print(top2)
+def score_transformation(score, k):
+    """
+    Transforms the recommendation scores and returns the top-k highest rated movie ids or titles.
+    
+    Args:
+        score (pd.Series): The recommendation scores for movies.
+        k (int): The number of top recommendations to return.
+    
+    Returns:
+        pd.Series: The top-k highest rated movie ids or titles.
+    """
+    recommendations = score.sort_values(ascending=False)
+    amin, amax = min(recommendations), max(recommendations)
+    for i, val in enumerate(recommendations):
+        recommendations[i] = (val - amin) / (amax - amin)
+    # Return the top-k highest rated movie ids or titles
+    return recommendations[:k]
+
 
 
 
