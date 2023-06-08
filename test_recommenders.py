bf5acb10f1ea25e8542af530a6341e17d035ba6e
Berkay <berkay.bb.bozkurt@gmail.com>
2023-06-08 09:27:14 +0200

    test added

--- test_recommenders.py
+++ test_recommenders.py
@@ -1,33 +1,36 @@
+"""TODO
+
+Write a program that checks that recommenders works as expected
+
+We will use pytest
+to install pytest run in the terminal 
++ pip install pytest
+/ conda install pytest
+
+TDD (Test-driven-development)  cycle:
+
+0. Make an Hypothesis:
+    the units/programs work
+1. Write test that fails (to disprove the Hypothesis)
+2. Change the code so that the Hypothesis is re-established
+3. repeat 0-->2
+
 """
-In this script we define functions for the recommender web
-application
-"""
-import random
-from recommenders import recommend_nmf, recommend_neighborhood, score_transformation
-from utils import MOVIES, nmf_model, cos_sim_model
+from recommenders import MOVIES, random_recommender
+
+def test_movies_are_strings():
+    for movie in MOVIES:
+        assert isinstance(movie,str)
+
+def test_for_two_movies():
+    top2 = random_recommender(k=2)
+    assert len(top2) == 2
 
 
-def recommend_nmf(query, nmf_model, titles, k=10):
-    """This is an nmf-based recommender"""
-    return NotImplementedError
+def test_for_5_movies():
+    top5 = random_recommender(5)
+    assert len(top5) == 5
 
-def recommend_neighborhood(query, cos_sim_model, titles, k=10):
-    """This is an cosine-similarity-based recommender"""
-    return NotImplementedError
-
-
-def random_recommender(k=2):
-    if k > len(MOVIES):
-        print(f"Hey you exceed the allowed number of movies {len(MOVIES)}")
-        return []
-    else:
-        random.shuffle(MOVIES)
-        top_k = MOVIES[:k]
-        return top_k
-
-
-if __name__ == "__main__":
-    top2 = random_recommender()
-    print(top2)
-
-
+def test_return_0_if_k_10_larger():
+    top10= random_recommender(10)
+    assert len(top10) == 0
